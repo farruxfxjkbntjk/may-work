35430d4e59e56b9323710ded8056b16155382240
Farrux <85478598+farruxfxjkbntjk@users.noreply.github.com>
2021-07-24 00:37:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var ismingizni_kiriting = prompt('ismingizni kiriting');
+console.log('Mening ismim Sanjar')
+var yoshingizni_kiriting = prompt('yoshingizni kiriting');
+console.log('Men 26 yoshdaman ')
+var Misolni_yeching1 = prompt('Misolni yeching 6+3=?')
+console.log('Misol 1: 6 + 3 = 9' + ' Sizning javobingiz ' +  Misolni_yeching1)
+var Misolni_yeching2 = prompt('Misolni yeching 20-5=?')
+console.log('Misol 2: 20 - 5  = 15' + ' Sizning javobingiz ' + Misolni_yeching2)
+var Misolni_yeching3 = prompt('Misolni yeching 15*2=?')
+console.log('Misol 3: 15 * 2 = 30' + ' Sizning javobingiz ' + Misolni_yeching3)
+var Misolni_yeching4 = prompt('Misolni yeching 10%3=?')
+console.log('Misol 4: 10 % 3 =1' + ' Sizning javobingiz ' + Misolni_yeching4 )
+alert("Natijani ko'rish uchun konsolni oching")
